864306a976b4dd2d17cc745859ea97be7f8cd58e
romarmph <romarmph@gmail.com>
2022-06-08 14:39:02 +0800

    Remove unnecessary comments

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-/**
- * 
- *  TASK:
- *      Create a rock-paper-scissor game against computer.
- * 
- *  NOTE:
- *      Game is played on console.
- *      Game has two players: User and Computer
- *      User needs to input a hand to play
- *      Computer needs randomize hand
- */
-
-/**
- * 
- * Ask user "Start Game? "
- * If user inputs "no"
- *  - Exit the game
- * If user inputs "yes"
- *  - Start the game
- *  - Ask user "Pick a hand: "
- *  - Generate a random hand for computer
- *  - Determine the result
- *  - Tell the user the result
- *  Ask the user "Play again? "
- * If user inputs no "no" 
- *  - Exit the game
- * If user inputs "yes"
- *  - Repeat the game
- *
- */
-
 const computerPlay = () => {
     const HAND = ["rock", "paper", "scissors"];
     return HAND[Math.floor(Math.random() * 3)];
